2bd6c814713326a5fd161eb9da26db0441ed3454
Igor Rumiha <igorrumiha@gmail.com>
2024-04-07 19:30:35 +0200

    Form mapping: extend to Option, add support for Sequences of case classes.

--- modules/form-mapping/src/main/scala/net/lambdaserve/form/mapping/FormMapped.scala
+++ modules/form-mapping/src/main/scala/net/lambdaserve/form/mapping/FormMapped.scala
@@ -3,67 +3,80 @@ package net.lambdaserve.form.mapping
 import magnolia1.*
 
 trait FormMapped[T]:
-  def mapForm(m: Map[String, Seq[String]], prefix: String = ""): T
+  def mapForm(m: Map[String, IndexedSeq[String]], prefix: String = "", offset: Int = 0): T
 
 object FormMapped extends Derivation[FormMapped]:
-  private def extractString(m: Map[String, Seq[String]], prefix: String): String =
-    m.getOrElse(prefix, throw IllegalArgumentException(s"Field not found at path $prefix"))
-      .headOption
-      .getOrElse(throw IllegalArgumentException(s"No value found at path $prefix"))
+  private def extractString(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int = 0): String =
+    val entries = m.getOrElse(prefix, throw IllegalArgumentException(s"Field not found at path $prefix"))
+    if entries.length <= offset then
+      throw new IllegalArgumentException(s"Index out of bounds at path $prefix, offset $offset")
+    entries(offset)
 
   given FormMapped[Int] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): Int =
-      extractString(m, prefix).toInt
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Int =
+      extractString(m, prefix, offset).toInt
 
   given FormMapped[Float] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): Float =
-      extractString(m, prefix).toFloat
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Float =
+      extractString(m, prefix, offset).toFloat
 
   given FormMapped[Double] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): Double =
-      extractString(m, prefix).toDouble
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Double =
+      extractString(m, prefix, offset).toDouble
 
   given FormMapped[BigDecimal] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): BigDecimal =
-      BigDecimal(extractString(m, prefix))
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): BigDecimal =
+      BigDecimal(extractString(m, prefix, offset))
 
   given FormMapped[Boolean] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): Boolean =
-      extractString(m, prefix).toBoolean
-
-  given seq[T](using sfm: FormMapped[T]): FormMapped[Seq[T]] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): Seq[T] =
-      m.getOrElse(prefix, throw IllegalArgumentException(s"Field not found at path $prefix"))
-        .map { it =>
-          sfm.mapForm(Map("" -> Seq(it)))  // TODO Super-inefficient
-        }
-
-  given list[T](using sfm: FormMapped[Seq[T]]): FormMapped[List[T]] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): List[T] =
-      sfm.mapForm(m, prefix).toList
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Boolean =
+      extractString(m, prefix, offset).toBoolean
 
   given FormMapped[String] with
-    override def mapForm(m: Map[String, Seq[String]], prefix: String): String =
-      extractString(m, prefix)
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): String =
+      extractString(m, prefix, offset)
+
+  given seq[T](using sfm: FormMapped[T]): FormMapped[Seq[T]] with
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Seq[T] =
+      m.keys.find(path => path.startsWith(prefix)) match
+        case Some(key) => m(key).indices.map { idx =>
+          sfm.mapForm(m, prefix, idx)
+        }
+        case _ => throw IllegalArgumentException(s"Field not found at path $prefix")
+
+  given list[T](using sfm: FormMapped[Seq[T]]): FormMapped[List[T]] with
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): List[T] =
+      sfm.mapForm(m, prefix, offset).toList
+
+  given vec[T](using sfm: FormMapped[Seq[T]]): FormMapped[Vector[T]] with
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Vector[T] =
+      sfm.mapForm(m, prefix, offset).toVector
+
+  given option[T](using sfm: FormMapped[T]): FormMapped[Option[T]] with
+    override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): Option[T] =
+      m.keys.find(path => path.startsWith(prefix)) match
+        case Some(prefKey) if m(prefKey).isEmpty => None
+        case Some(prefKey) => Some(sfm.mapForm(m, prefix, offset))
+        case _ => None
 
   override def split[T](ctx: SealedTrait[FormMapped.Typeclass, T]): FormMapped.Typeclass[T] =
     new FormMapped[T]:
-      override def mapForm(m: Map[String, Seq[String]], prefix: String): T =
+      override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): T =
         val stringValue = extractString(m, prefix)
         val subtype = ctx.subtypes.find(_.typeInfo.short == stringValue).get
 
-        subtype.typeclass.mapForm(m, prefix)
+        subtype.typeclass.mapForm(m, prefix, offset)
 
   override def join[T](ctx: CaseClass[FormMapped.Typeclass, T]): FormMapped.Typeclass[T] =
     new FormMapped[T]:
-      override def mapForm(m: Map[String, Seq[String]], prefix: String): T =
+      override def mapForm(m: Map[String, IndexedSeq[String]], prefix: String, offset: Int): T =
         val allParams = ctx.parameters.map { param =>
           val currentPrefix =
             if prefix.isEmpty then
               param.label
             else
               s"$prefix.${param.label}"
-          param.typeclass.mapForm(m, currentPrefix)
+          param.typeclass.mapForm(m, currentPrefix, offset)
         }
 
         ctx.rawConstruct(allParams)
@@ -88,19 +101,25 @@ def main(args: String*): Unit =
   object Months:
     given formMapper: FormMapped[Months] = FormMapped.derived
 
-  case class MyForm(firstName: String, lastName: String, points: List[Int], bornIn: Months)
+  case class Pet(name: String, breed: String)
+  object Pet:
+    given formMapper: FormMapped[Pet] = FormMapped.derived
+
+  case class MyForm(firstName: String, lastName: String, points: List[Int], bornIn: Months, pet: List[Pet])
   object MyForm:
     given formMapper: FormMapped[MyForm] = FormMapped.derived
 
-  def printForm[F](formData: Map[String, Seq[String]])(using f: FormMapped[MyForm]): Unit =
+  def printForm[F](formData: Map[String, IndexedSeq[String]])(using f: FormMapped[MyForm]): Unit =
     val mapped = f.mapForm(formData)
     println(mapped)
 
   printForm(
     Map(
-      "firstName" -> Seq("Pero"),
-      "lastName" -> Seq("Milikona"),
-      "points" -> Seq("12", "44", "52"),
-      "bornIn" -> Seq("Oct")
+      "firstName" -> IndexedSeq("Pero"),
+      "lastName" -> IndexedSeq("Milikona"),
+      "points" -> IndexedSeq("12", "44", "52"),
+      "bornIn" -> IndexedSeq("Oct"),
+      "pet.name" -> IndexedSeq("Max", "Milly"),
+      "pet.breed" -> IndexedSeq("Dog", "Cat")
     )
   )
