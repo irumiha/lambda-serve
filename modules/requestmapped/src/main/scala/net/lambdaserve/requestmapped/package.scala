e897813b2b0c61ab2d8abbc23fb4624beabe23e5
Igor Rumiha <igorrumiha@gmail.com>
2024-04-24 13:50:01 +0000

    Add more request mappings.

--- modules/requestmapped/src/main/scala/net/lambdaserve/requestmapped/package.scala
+++ modules/requestmapped/src/main/scala/net/lambdaserve/requestmapped/package.scala
@@ -6,60 +6,96 @@ import net.lambdaserve.mapextract.MapExtract
 
 def joinMaps(r: Request) = r.pathParams ++ r.header.query ++ r.form ++ r.headers
 
-def mapped[T](h: (T, Request) => Response)(using
-  m: MapExtract[T]
-): Request => Response =
-  request =>
-    val handlerParam = m.projectMap(joinMaps(request))
-    h(handlerParam, request)
-
-def mapped[T1, T2](
-  h: (T1, T2) => Response
-)(using m1: MapExtract[T1], m2: MapExtract[T2]): Request => Response =
-  request =>
-    val joinedMaps = joinMaps(request)
-    val p1         = m1.projectMap(joinedMaps)
-    val p2         = m2.projectMap(joinedMaps)
-    h(p1, p2)
-
-def mapped[T1, T2](
-  h: (T1, T2, Request) => Response
-)(using m1: MapExtract[T1], m2: MapExtract[T2]): Request => Response =
-  request =>
-    val joinedMaps = joinMaps(request)
-    val p1         = m1.projectMap(joinedMaps)
-    val p2         = m2.projectMap(joinedMaps)
-    h(p1, p2, request)
-
-def mapped[T1, T2, T3](h: (T1, T2, T3) => Response)(using
-  m1: MapExtract[T1],
-  m2: MapExtract[T2],
-  m3: MapExtract[T3]
-): Request => Response =
-  request =>
-    val joinedMaps = joinMaps(request)
-    val p1         = m1.projectMap(joinedMaps)
-    val p2         = m2.projectMap(joinedMaps)
-    val p3         = m3.projectMap(joinedMaps)
-    h(p1, p2, p3)
-
-def mapped[T1, T2, T3](h: (T1, T2, T3, Request) => Response)(using
-  m1: MapExtract[T1],
-  m2: MapExtract[T2],
-  m3: MapExtract[T3]
-): Request => Response =
-  request =>
-    val joinedMaps = joinMaps(request)
-    val p1         = m1.projectMap(joinedMaps)
-    val p2         = m2.projectMap(joinedMaps)
-    val p3         = m3.projectMap(joinedMaps)
-    h(p1, p2, p3, request)
+type Combined[R] = MapExtract[R] | EntityDecoder[R]
 
 def mapped[T](h: T => Response)(using
-  m: MapExtract[T] | EntityDecoder[T]
+  m: Combined[T]
 ): Request => Response =
   request =>
     val handlerParam = m match
       case me: MapExtract[T]     => me.projectMap(joinMaps(request))
       case jvc: EntityDecoder[T] => jvc.readBody(request)
     h(handlerParam)
+
+def mapped[T](h: (T, Request) => Response)(using
+  m: Combined[T]
+): Request => Response =
+  request =>
+    val handlerParam = m match
+      case me: MapExtract[T]     => me.projectMap(joinMaps(request))
+      case jvc: EntityDecoder[T] => jvc.readBody(request)
+    h(handlerParam, request)    
+
+def mapped[T1, T2](
+  h: (T1, T2) => Response
+)(using m1: Combined[T1], m2: Combined[T2]): Request => Response =
+  request =>
+    val joinedMaps = joinMaps(request)
+    
+    val p1 = m1 match
+      case me: MapExtract[T1]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T1] => jvc.readBody(request)
+    
+    val p2         = m2 match
+      case me: MapExtract[T2]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T2] => jvc.readBody(request)
+
+    h(p1, p2)
+
+def mapped[T1, T2](
+  h: (T1, T2, Request) => Response
+)(using m1: Combined[T1], m2: Combined[T2]): Request => Response =
+  request =>
+    val joinedMaps = joinMaps(request)
+    
+    val p1 = m1 match
+      case me: MapExtract[T1]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T1] => jvc.readBody(request)
+    
+    val p2         = m2 match
+      case me: MapExtract[T2]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T2] => jvc.readBody(request)
+
+    h(p1, p2, request)
+
+def mapped[T1, T2, T3](h: (T1, T2, T3) => Response)(using
+ m1: Combined[T1], 
+ m2: Combined[T2], 
+ m3: Combined[T3]
+): Request => Response =
+  request =>
+    val joinedMaps = joinMaps(request)
+    
+    val p1 = m1 match
+      case me: MapExtract[T1]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T1] => jvc.readBody(request)
+    
+    val p2         = m2 match
+      case me: MapExtract[T2]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T2] => jvc.readBody(request)
+
+    val p3         = m3 match
+      case me: MapExtract[T3]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T3] => jvc.readBody(request)
+
+    h(p1, p2, p3)
+
+def mapped[T1, T2, T3](h: (T1, T2, T3, Request) => Response)(using
+ m1: Combined[T1], 
+ m2: Combined[T2], 
+ m3: Combined[T3]
+): Request => Response =
+  request =>
+    val joinedMaps = joinMaps(request)
+    val p1 = m1 match
+      case me: MapExtract[T1]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T1] => jvc.readBody(request)
+    
+    val p2         = m2 match
+      case me: MapExtract[T2]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T2] => jvc.readBody(request)
+
+    val p3         = m3 match
+      case me: MapExtract[T3]     => me.projectMap(joinedMaps)
+      case jvc: EntityDecoder[T3] => jvc.readBody(request)
+    h(p1, p2, p3, request)
